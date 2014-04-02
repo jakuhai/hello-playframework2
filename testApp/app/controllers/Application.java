89ce1a55f9bb16641750690a6f68baec0323570a
jakuhai <infityjp@yahoo.co.jp>
2014-04-02 14:06:51 +0900

    addアクション追加

--- testApp/app/controllers/Application.java
+++ testApp/app/controllers/Application.java
@@ -1,16 +1,21 @@
 package controllers;
 
-import play.*;
-import play.data.*;
-import static play.data.Form.*;
-import play.mvc.*;
+import java.util.List;
 
-import views.html.*;
-
-import java.util.*;
-import models.*;
+import models.Message;
+import play.data.Form;
+import play.data.validation.Constraints.Required;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.add;
+import views.html.index;
 
 public class Application extends Controller {
+
+	public static class FindForm {
+		@Required
+		public String input;
+	}
 
 	// Form用の内部クラス
 	public static class SampleForm{
@@ -21,26 +26,27 @@ public class Application extends Controller {
 
 	// ルートにアクセスした際のAction
     public static Result index() {
-    
+
     	List<Message> datas = Message.find.all();
-    
+
         return ok(index.render("データベースサンプル。",datas));
     }
-    
-    // /sendにアクセスした際のAction
-    /*public static Result send(){
-    	Form<SampleForm> f = form(SampleForm.class).bindFromRequest();
-    	if( !f.hasErrors() ){
-    	
-    		SampleForm data = f.get();
-    		String msg = "you typed: " + data.message;
-    		return ok(index.render(msg,f));
-    	
-    	} else {
-    	
-    		return badRequest(index.render("ERROR", form(SampleForm.class)));
-    	
-    	}
-    }*/
+
+    public static Result add(){
+    	Form<Message> f = new Form<Message>(Message.class);
+    	return ok(add.render("投稿フォーム",f));
+    }
+
+ // /createにアクセスした際のAction
+ 	public static Result create(){
+ 		Form<Message> f = new Form<Message>(Message.class).bindFromRequest();
+ 		if (!f.hasErrors()){
+ 			Message data = f.get();
+ 			data.save();
+ 			return redirect("/");
+ 		} else {
+ 			return badRequest(add.render("ERROR", f));
+ 		}
+ 	}
 
 }
