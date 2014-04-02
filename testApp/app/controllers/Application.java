f2c93091c661d3afc66635b7d1169bd016db5960
jakuhai <infityjp@yahoo.co.jp>
2014-04-02 15:26:24 +0900

    追加、編集、削除機能

--- testApp/app/controllers/Application.java
+++ testApp/app/controllers/Application.java
@@ -8,7 +8,10 @@ import play.data.validation.Constraints.Required;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.add;
+import views.html.delete;
+import views.html.edit;
 import views.html.index;
+import views.html.item;
 
 public class Application extends Controller {
 
@@ -37,7 +40,7 @@ public class Application extends Controller {
     	return ok(add.render("投稿フォーム",f));
     }
 
- // /createにアクセスした際のAction
+    // /createにアクセスした際のAction
  	public static Result create(){
  		Form<Message> f = new Form<Message>(Message.class).bindFromRequest();
  		if (!f.hasErrors()){
@@ -47,6 +50,66 @@ public class Application extends Controller {
  		} else {
  			return badRequest(add.render("ERROR", f));
  		}
+ 	}
+
+ 	public static Result setitem(){
+ 		Form<Message> f = new Form<Message>(Message.class);
+ 		return ok(item.render("ID番号を入力。",f));
+ 	}
+
+ 	public static Result edit(){
+ 		Form<Message> f = new Form<Message>(Message.class).bindFromRequest();
+ 		if (!f.hasErrors()){
+ 			Message obj = f.get();
+
+ 			Long id = obj.id;
+ 			obj = Message.find.byId(id);
+ 			if(obj != null){
+ 				f = new Form<Message>(Message.class).fill(obj);
+ 				return ok(edit.render("ID="+ id + "の投稿を編集。", f));
+ 			}else{
+ 				return ok(item.render("ERROR：IDの投稿が見つかりません", f));
+ 			}
+
+ 		} else {
+ 			return badRequest(edit.render("ERROR：入力に問題があります。", f));
+ 		}
+ 	}
+
+ 	public static Result update(){
+ 		Form<Message> f = new Form<Message>(Message.class).bindFromRequest();
+ 		if (!f.hasErrors()){
+ 			Message obj = f.get();
+ 			obj.update();
+ 			return redirect("/");
+ 		} else {
+ 			return badRequest(edit.render("ERROR：入力に問題があります。", f));
+ 		}
+ 	}
+
+ 	public static Result delete(){
+ 		Form<Message> f = new Form<Message>(Message.class);
+ 		return ok(delete.render("削除するID番号。",f));
+ 	}
+
+ 	public static Result remove(){
+
+ 		Form<Message> f = new Form<Message>(Message.class).bindFromRequest();
+ 		if (!f.hasErrors()){
+ 			Message obj = f.get();
+ 			obj = Message.find.byId(obj.id);
+ 			if(obj != null){
+ 				obj.delete();
+ 				return redirect("/item");
+ 			}else{
+ 				return ok(delete.render("ERROR：IDの投稿が見つかりません", f));
+ 			}
+
+
+ 		} else {
+ 			return badRequest(delete.render("ERROR：入力に問題があります。", f));
+ 		}
+
  	}
 
 }
